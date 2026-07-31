16f449d001300c2d4851aba4e7cfaa5eca4666cf
Oscar <oscaralvarezx2@gmail.com>
2026-07-31 19:44:38 +0200

    feat: añadida seccion de servicios

---
+++ src/data/services.ts
@@ -0,0 +1,23 @@
+export const services = [
+  {
+    id: 1,
+    title: "Grabación",
+    image: "/images/grabacion.jpg",
+	description: "Grabación en cabina",
+    price: 25,
+  },
+  {
+    id: 2,
+    title: "Mezcla y mastering",
+    image: "/images/mastering.jpg",
+	description: "Trabajo sobre pistas",
+    price: 10,
+  },
+  {
+    id: 3,
+    title: "Videoclips",
+    image: "/images/videoclip.jpg",
+	description: "Rodaje",
+    price: 90,
+  },
+];
