2294a0c7ead482a53d137260ba80f1993266a6a6
Oscar <oscaralvarezx2@gmail.com>
2026-07-31 20:30:35 +0200

    fix: corregidas  rutas en GitHub Pages

--- src/data/services.ts
+++ src/data/services.ts
@@ -1,23 +1,27 @@
+import grabacion from '../assets/grabacion.jpg';
+import mastering from '../assets/mastering.jpg';
+import videoclip from '../assets/videoclip.jpg';
+
 export const services = [
   {
     id: 1,
     title: "Grabación",
-    image: "grabacion.jpg",
-	description: "Grabación en cabina",
+    image: grabacion,
+    description: "Grabación en cabina",
     price: 25,
   },
   {
     id: 2,
     title: "Mezcla y mastering",
-    image: "mastering.jpg",
-	description: "Trabajo sobre pistas",
+    image: mastering,
+    description: "Mezcla y mastering",
     price: 10,
   },
   {
     id: 3,
-    title: "Videoclips",
-    image: "videoclip.jpg",
-	description: "Rodaje",
+    title: "Videoclip",
+    image: videoclip,
+    description: "Rodaje",
     price: 90,
   },
 ];
